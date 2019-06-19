4166cd51eac439e6a387da56b295aac52391f049
Nicolas Long <nicolas.long@theguardian.com>
2019-06-19 14:18:44 +0100

    Process all quasis

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-// Thanks to: https://github.com/cuth/postcss-pxtorem/blob/master/lib/pixel-unit-regex.js#L9
-var pxRegex = /"[^"]+"|'[^']+'|url\([^\)]+\)|(\d*\.?\d+)px/g;
+var pxRegex = /(\d*\.?\d+)px/g;
 
-function replace(match, p1) {
-    return p1 / 16 + "rem";
+function replacePx(match, p1) {
+    return (p1 / 16).toString() + "rem";
 }
 
+function transform(quasis) {
+    ["cooked", "raw"].forEach(function(type) {
+        quasis.forEach(function(element, i) {
+            element.value[type] = element.value[type].replace(
+                pxRegex,
+                replacePx
+            );
+        });
+    });
+}
+
+// To log on errors, use DEBUG=babel:* when running via node
 function PxToRem(babel) {
     return {
         name: "px-to-rem",
@@ -14,11 +25,8 @@ function PxToRem(babel) {
                     return;
                 }
 
-                var cooked = path.node.quasi.quasis[0].value.cooked;
-                path.node.quasi.quasis[0].value.raw = cooked.replace(
-                    pxRegex,
-                    replace
-                );
+                var quasis = path.node.quasi.quasis;
+                transform(quasis);
             }
         }
     };
